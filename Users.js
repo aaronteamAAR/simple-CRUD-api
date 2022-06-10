7cd11fae603b78e6309582d72c4e9e015d11638a
Aaron Chris <rooneychris83@gmail.com>
2022-06-10 22:12:50 +0100

    CRUD

---
+++ Users.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        id: 1,
+        name: 'aaron chris',
+        email: 'aaronchrischuku@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Hubert chris',
+        email: 'hubertchrischuku@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'debbie chris',
+        email: 'debrischrischuku@gmail.com'
+    },
+]
+
+module.exports = users;
